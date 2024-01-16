50836560bc94048d75f24d452be1132f720abc8e
Damir Mihajlović <74556243+damirko881@users.noreply.github.com>
2024-01-16 16:02:44 +0100

    Add files via upload

---
+++ src/main/java/com/example/highlife/HighLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.highlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/highlife/HighLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.highlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
